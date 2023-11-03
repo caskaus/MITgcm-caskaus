1cd83de2c4fada42d8a6db89e7f5a6ec88ca0cc0
caskaus <105206744+caskaus@users.noreply.github.com>
2023-11-03 18:25:57 +0530

    Create modules.sh
    Load these modules before working 
    
    ```source modules.sh```

---
+++ modules.sh
@@ -0,0 +1,7 @@
+module load pbs
+module load openmpi/4.1.5
+module load netcdf/4.9.2
+module load intel-compiler/2021.10.0
+module load hdf5/1.12.1
+module load intel-mkl/2022.2.0
+module load python3/3.11.0
